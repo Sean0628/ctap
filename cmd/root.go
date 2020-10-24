9226950b98fc82d651b5e18a0ed3e45b17e63e52
Sho ITO <i.sho.0628@gmail.com>
2020-10-25 00:36:10 +0900

    tmp

--- cmd/root.go
+++ cmd/root.go
@@ -13,15 +13,15 @@ var (
     Short: "CLI crontab parser",
     Long: `ctap is a CLI crontab parser written in Go.`,
     RunE: func(cmd *cobra.Command, args []string) error {
-			if version {
-				cmd.Print(GetVersionDisplay())
-				return nil
-			}
+      if version {
+        cmd.Print(GetVersionDisplay())
+        return nil
+      }
       if len(args) < 1 {
         cmd.Println(cmd.UsageString())
         return nil
       }
-
+      cmd.Print(GetParsedExpressionDisplay(args))
       return nil
     },
 }
@@ -35,9 +35,9 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().StringVarP(&locale, "locale", "l", "en", "Prints description in the specified locale")
+  rootCmd.Flags().StringVarP(&locale, "locale", "l", "en", "Prints description in the specified locale")
   rootCmd.Flags().BoolVarP(&version, "version", "V", false, "Prints version information")
-	rootCmd.Flags().BoolVarP(&dayOfWeek, "dow-starts-at-one", "d", false, "Is day of the week starts at 1 (Monday-Sunday: 1-7)")
-	rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Prints description in verbose format")
-	rootCmd.Flags().BoolVar(&format24, "24-hour", false, "Prints description in 24 hour time format")
+  rootCmd.Flags().BoolVarP(&dayOfWeek, "dow-starts-at-one", "d", false, "Is day of the week starts at 1 (Monday-Sunday: 1-7)")
+  rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Prints description in verbose format")
+  rootCmd.Flags().BoolVar(&format24, "24-hour", false, "Prints description in 24 hour time format")
 }
