e36ffb13dc4b434ba62c8ae5ae51a7914e3879a4
Sho ITO <i.sho.0628@gmail.com>
2020-11-01 11:25:27 +0900

    test

--- cmd/parse.go
+++ cmd/parse.go
@@ -1,44 +1,44 @@
 package cmd
 
 import (
-	"fmt"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
+  "fmt"
+  "io/ioutil"
+  "path/filepath"
+  "strings"
 )
 
 func GetParsedExpressionDisplay(args []string) string {
-	expr := args[0]
-	outputFilePath := strings.TrimSpace(fOutputFile)
+  expr := args[0]
+  outputFilePath := strings.TrimSpace(fOutputFile)
 
-	loc, err := GetParseLocale()
-	if err != nil {
-		return fmt.Sprintln(err.Error())
-	}
+  loc, err := GetParseLocale()
+  if err != nil {
+    return fmt.Sprintln(err.Error())
+  }
 
-	exprDesc, err := GetExprDescriptor(loc)
-	if err != nil {
-		return fmt.Sprintln(err.Error())
-	}
+  exprDesc, err := GetExprDescriptor(loc)
+  if err != nil {
+    return fmt.Sprintln(err.Error())
+  }
 
-	desc, err := exprDesc.ToDescription(args[0], loc)
-	if err != nil {
-		return fmt.Sprintln(err.Error())
-	}
+  desc, err := exprDesc.ToDescription(args[0], loc)
+  if err != nil {
+    return fmt.Sprintln(err.Error())
+  }
 
-	results := fmt.Sprintf("%s: %s\n", expr, desc)
+  results := fmt.Sprintf("%s: %s\n", expr, desc)
 
-	if len(outputFilePath) > 0 {
-		message := []byte(results)
+  if len(outputFilePath) > 0 {
+    message := []byte(results)
 
-		absolutePath, _ := filepath.Abs(outputFilePath)
-		err := ioutil.WriteFile(absolutePath, message, 0644)
-		if err != nil {
-			return fmt.Sprintln(err.Error())
-		}
+    absolutePath, _ := filepath.Abs(outputFilePath)
+    err := ioutil.WriteFile(absolutePath, message, 0644)
+    if err != nil {
+      return fmt.Sprintln(err.Error())
+    }
 
-		return fmt.Sprintf("ctap: the results are printed out to %s.\n", outputFilePath)
-	}
+    return fmt.Sprintf("ctap: the results are printed out to %s.\n", outputFilePath)
+  }
 
-	return results
+  return results
 }
